9aae34047421e6f16131dcbc6d1cd56e2729649d
Bahram Jahanshahi <jahanshahi@amnpardaz.com>
2019-12-05 16:48:19 +0330

    Initial

---
+++ src/main/resources/scholarship_scholarship.sql
@@ -0,0 +1,1 @@
+INSERT INTO scholarship.scholarship (id, status, name, family, nationalCode, lastUni, lastDegree, lastField, lastScore, applyUni, applyDegree, applyField, applyDate) VALUES (1, 'RequestedByStudent', 'Bahram', 'Jahanshahi', '1234567890', 'Beheshti', 'Bachelor', 'Software Engineer', 14.5, 'Sharif', 'Master', 'Software Engineer', '1399-07-1');
