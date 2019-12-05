1e45d571f9d04651b5b8caf245181e6f7dc96d60
Bahram Jahanshahi <jahanshahi@amnpardaz.com>
2019-12-05 16:40:28 +0330

    Initial

--- src/main/java/ir/mctab/java32/projects/scholarshipmanagement/features/scholarshipverification/impl/FindScholarshipBySupervisorUseCase.java
+++
@@ -1,7 +0,0 @@
-package ir.mctab.java32.projects.scholarshipmanagement.features.scholarshipverification.impl;
-
-import ir.mctab.java32.projects.scholarshipmanagement.core.annotations.UseCase;
-
-@UseCase
-public interface FindScholarshipBySupervisorUseCase {
-}
