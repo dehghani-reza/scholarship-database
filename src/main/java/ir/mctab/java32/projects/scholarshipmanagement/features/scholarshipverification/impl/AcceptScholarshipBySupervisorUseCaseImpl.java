d170c73c1ff6d6d1648357d57039d6e2cc3be424
Reza <dehghan7447@gmail.com>
2019-12-12 11:48:13 +0330

    log

--- src/main/java/ir/mctab/java32/projects/scholarshipmanagement/features/scholarshipverification/impl/AcceptScholarshipBySupervisorUseCaseImpl.java
+++ src/main/java/ir/mctab/java32/projects/scholarshipmanagement/features/scholarshipverification/impl/AcceptScholarshipBySupervisorUseCaseImpl.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 @Service
 public class AcceptScholarshipBySupervisorUseCaseImpl implements AcceptScholarshipBySupervisorUseCase {
